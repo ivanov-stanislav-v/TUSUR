afe69c41aa999d167856609dd36c630c97d4566c
Станислав <ivanov.stanislav.v@gmail.com>
2018-08-29 00:11:49 +0700

    hello

--- src/main/java/ru/tusur/udo/App.java
+++ src/main/java/ru/tusur/udo/App.java
@@ -1,6 +1,7 @@
 package ru.tusur.udo;
 
-public class App {
+public class App
+{
     public static void main (String[] args) {
         Greeting.toConsole();
     }
--- src/main/java/ru/tusur/udo/Greeting.java
+++ src/main/java/ru/tusur/udo/Greeting.java
@@ -2,6 +2,6 @@ package ru.tusur.udo;
 
 public class Greeting {
     public static void toConsole() {
-
+        System.out.println("Hello");
     }
 }
